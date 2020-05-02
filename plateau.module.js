5a82803e00cd450e54b2aad59afcc1b33ced6678
Invisible-Media <invisible-media@outlook.com>
2020-05-02 14:20:31 +0200

    added pacman3D.html ; walls inexact

---
+++ plateau.module.js
@@ -0,0 +1,45 @@
+let gameplay = [
+	{
+		"level" : 1,
+		"cakeCounter" : 0,
+		"wall" : new Image(),
+		"cake" : new Image(),
+		"board" : {
+			"x_size" : 17,
+			"y_size" : 27,
+			"x_length" : 30,
+			"y_length" : 30,
+			"cells" : [
+				"WALL", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "WALL",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+				"CAKE", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "CAKE",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL",
+				"WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+				"CAKE", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "WALL", "WALL", "WALL", "CAKE", "WALL", "CAKE", "WALL", "WALL", "WALL", "CAKE", "WALL", "CAKE", "WALL", "WALL", "WALL", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "WALL", "WALL", "WALL", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"WALL", "CAKE", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "CAKE", "WALL",
+				"WALL", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "WALL",
+				"CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE",
+				"WALL", "CAKE", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "CAKE", "WALL",
+				"CAKE", "CAKE", "CAKE", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE",
+				"CAKE", "WALL", "WALL", "CAKE", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "WALL", "CAKE", "WALL", "WALL", "CAKE",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+				"CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE", "CAKE", "CAKE", "CAKE", "WALL", "CAKE", "CAKE",
+			]
+		}
+	}
+];
+
+export const GamePlay = gameplay;
